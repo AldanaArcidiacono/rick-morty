477599838f0aa8c37675996dabcf3c43903b0dc1
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2023-02-07 17:15:54 +0100

    🔧 Add bootstrap config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
